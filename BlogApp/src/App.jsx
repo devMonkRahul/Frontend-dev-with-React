863402492ce47f2b9d6c1c4f2df459933e19a644
N8MARERahul <rahulpal2023iem@gmail.com>
2024-12-20 15:26:03 +0530

    blog app initial commit

---
+++ BlogApp/src/App.jsx
@@ -0,0 +1,12 @@
+import config from "./conf/conf";
+
+function App() {  
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-orange-400">React + Vite</h1>
+    </>
+  )
+}
+
+export default App
+ 
