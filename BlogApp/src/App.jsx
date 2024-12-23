c6ff9b15258b124ddeced3051274227fa4382e40
N8MARERahul <nightmarecoding0001@gmail.com>
2024-12-23 10:34:05 +0530

    components created

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,11 +1,43 @@
 import config from "./conf/conf";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/features/authSlice"
+import { Footer, Header } from "./components";
 
 function App() {  
-  return (
+
+  const [ loading, setLoading ] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => { setLoading(false) });
+  }, []);
+
+  return !loading ? (
     <>
-      <h1 className="text-3xl font-bold underline text-orange-400">React + Vite</h1>
+      <div className="min-h-screen flex flex-wrap content-between items-center justify-center bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+            {/* <Outlet /> */}
+            TODO: Add routes
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  ) : (
+    <div className="text-3xl font-bold underline text-red-400 flex justify-center items-center w-full h-screen">Loading...</div>
+  );
 }
 
 export default App
