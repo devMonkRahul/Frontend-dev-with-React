04aaf9ba83c0836698ebd4fca895ca5dbc7e4c93
N8MARERahul <rahulpal2023iem@gmail.com>
2025-01-02 18:04:39 +0530

    add Card component; enhance Select component with ref forwarding and improved styling

--- BlogApp/src/components/Select/Select.jsx
+++ BlogApp/src/components/Select/Select.jsx
@@ -10,11 +10,19 @@ function Select({
   return (
     <div className='w-full'>
         {label && <label htmlFor={id} className={`${className}`}></label>}
-        <select {...props}>
-
+        <select 
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border-gray-200 w-full ${className}`}        >
+          {options?.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
     </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
