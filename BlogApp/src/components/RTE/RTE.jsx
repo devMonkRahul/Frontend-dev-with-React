6af5183d59c1fcc07fc34cfa57e90ecf2ce547a7
N8MARERahul <rahulpal2023iem@gmail.com>
2025-01-06 18:36:28 +0530

    enhance RTE component to support controlled input with react-hook-form

--- BlogApp/src/components/RTE/RTE.jsx
+++ BlogApp/src/components/RTE/RTE.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import { Editor } from '@tinymce/tinymce-react'
+import { Controller } from 'react-hook-form'
 
-export default function RTE() {
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
-    <Editor />
+    <div className='w-full'>
+      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+      <Controller 
+      name={name || "Content"}
+      control={control}
+      render={({ field: {onChange} }) => (
+        <Editor 
+        initialValue={defaultValue}
+        init={{
+          initialValue: defaultValue,
+          height: 500,
+          menubar: true,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+          ],
+          toolbar:
+            'undo redo | formatselect | bold italic backcolor | \
+            alignleft aligncenter alignright alignjustify | \
+            bullist numlist outdent indent | removeformat | help'
+        }}
+        />
+      )}
+      />
+    </div>
   )
 }
